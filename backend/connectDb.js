bdf658f16892344bf94c71a409b486056b975cdc
Tanishq Sharma <sharma.tanishq9999@gmail.com>
2022-05-14 16:02:45 +0530

    Delete backend directory

--- backend/connectDb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const mongooseURI=process.env.ATLAS
-
-const connectToMongoose =()=>{
-    mongoose.connect(mongooseURI,{keepAlive:true},()=>console.log('Connected to mongoDB succesfully'));
-}
-
-module.exports =connectToMongoose;
